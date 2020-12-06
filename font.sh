741789f171c9e4b5b79e69731bd341d84c4f677d
konnano <73874687+konnano@users.noreply.github.com>
2020-12-06 22:42:39 +0900

    Update font.sh

--- font.sh
+++ font.sh
@@ -2,7 +2,7 @@
 wget -q --no-check-certificate -P ~/.BREW_LIST https://github.com/Homebrew/homebrew-cask-fonts/archive/master.zip || exit
 unzip -q ~/.BREW_LIST/master.zip -d ~/.BREW_LIST
 ls ~/.BREW_LIST/homebrew-cask-fonts-master/Casks > ~/.BREW_LIST/Q_FONT.txt
-rm ~/.BREW_LIST/master.zip ~/.BREW_LIST/master.zip.[1-9] ~/.BREW_LIST/master.zip.[1-9].[1-9] 2>/dev/null
+rm ~/.BREW_LIST/master.zip ~/.BREW_LIST/master.zip.* 2>/dev/null
 rm -rf ~/.BREW_LIST/homebrew-cask-fonts-master
 
 if [ ! -s ~/.BREW_LIST/Q_FONT.txt ]
