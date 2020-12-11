d9f3c4ae635192a07bd7eaee17b479ae3327f904
konnano <73874687+konnano@users.noreply.github.com>
2020-12-11 18:45:55 +0900

    Update font.sh

--- font.sh
+++ font.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
+if LS=(`ls -dlT ~/.BREW_LIST/LOCK 2>/dev/null`);then
+TI=(`date +"%Y %-m %-d"`)
+if [ ${TI[0]} -gt ${LS[8]} -o ${TI[1]} -gt ${LS[5]} -o ${TI[2]} -gt ${LS[6]} ];then
+rmdir ~/.BREW_LIST/LOCK
+fi
+fi
+
 if ! mkdir ~/.BREW_LIST/LOCK 2>/dev/null
 then
 exit
 fi
 
 trap '
-rm ~/.BREW_LIST/master.zip  2>/dev/null
+rm ~/.BREW_LIST/master.zip 2>/dev/null
 rm -rf ~/.BREW_LIST/homebrew-cask-fonts-master
-rmdir ~/.BREW_LIST/LOCK  2>/dev/null
+rmdir ~/.BREW_LIST/LOCK 2>/dev/null
 exit' 1 2 3 15 20
 
 wget -q --no-check-certificate -P ~/.BREW_LIST https://github.com/Homebrew/homebrew-cask-fonts/archive/master.zip || exit
-unzip -qo ~/.BREW_LIST/master.zip -d ~/.BREW_LIST
+unzip -q ~/.BREW_LIST/master.zip -d ~/.BREW_LIST
 ls ~/.BREW_LIST/homebrew-cask-fonts-master/Casks > ~/.BREW_LIST/Q_FONT.txt
 rm ~/.BREW_LIST/master.zip
 rm -rf ~/.BREW_LIST/homebrew-cask-fonts-master
