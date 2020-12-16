f0b23f9c88a701dcf68311c46654c804f3c3d692
konnano <73874687+konnano@users.noreply.github.com>
2020-12-16 22:01:03 +0900

    Update font.sh

--- font.sh
+++ font.sh
@@ -3,8 +3,9 @@
 if ! mkdir ~/.BREW_LIST/LOCK 2>/dev/null;then
  if LS=(`ls -dlT ~/.BREW_LIST/LOCK 2>/dev/null`);then
   TI=(`date +"%Y %-m %-d"`)
-  [ ${TI[0]} -gt ${LS[8]} -o ${TI[1]} -gt ${LS[5]} -o ${TI[2]} -gt ${LS[6]} ] &&\
-  rm -rf ~/.BREW_LIST/LOCK
+  if[ ${TI[0]} -gt ${LS[8]} -o ${TI[1]} -gt ${LS[5]} -o ${TI[2]} -gt ${LS[6]} ];then
+   rm -rf ~/.BREW_LIST/LOCK
+  fi
  fi
 exit
 fi
