1cc6c4aff95462143c99c52c85748e8430e7a617
konnano <73874687+konnano@users.noreply.github.com>
2021-06-08 23:12:14 +0900

    Update brew_list.pl

--- brew_list.pl
+++ brew_list.pl
@@ -45,7 +45,7 @@ if( $AR[0] eq '-l' ){      $name = $re;  $re->{'LIST'}  = 1;
 }elsif( $AR[0] eq '-' ){   $re->{'BL'} = $ref->{'BL'} = 1;
 }else{  Died_1(); }
 
- exit if $AR[1] and $AR[1] =~ m|^/\^?[\+\*\?\(\)\[\|]|;
+ exit if $AR[1] and $AR[1] =~ m!^/[\+\*\?\(\)\[\|]|^/\^[\+\*\[]/!;
 
 if( $re->{'COM'} or $AR[1] and $name->{'LIST'} ){
  $AR[1] ? $re->{'STDI'} = lc $AR[1] : Died_1();
