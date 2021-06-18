b48612f710b953be4ba00cb0e5cd81eb1f6dab14
konnano <73874687+konnano@users.noreply.github.com>
2021-06-18 18:45:24 +0900

    Update font.sh

--- font.sh
+++ font.sh
@@ -16,71 +16,69 @@ rm -f ~/.BREW_LIST/Q_FONT.txt ~/.BREW_LIST/Q_DRIV.txt
 rm -rf ~/.BREW_LIST/LOCK
 exit' 1 2 3 15 20
 
+ test='0'
+if [ `uname` = Darwin ];then
+
 curl -so ~/.BREW_LIST/Q_BREW.html https://formulae.brew.sh/formula/index.html ||\
+ { rm -rf ~/.BREW_LIST/LOCK; exit; }
+curl -so ~/.BREW_LIST/Q_CASK.html https://formulae.brew.sh/cask/index.html ||\
  { rm -rf ~/.BREW_LIST/LOCK; exit; }
 curl -sLo ~/.BREW_LIST/master1.zip https://github.com/Homebrew/homebrew-cask-fonts/archive/master.zip ||\
  { rm -rf ~/.BREW_LIST/LOCK; exit; }
-
-if [ `uname` = Darwin ];then
-curl -so ~/.BREW_LIST/Q_CASK.html https://formulae.brew.sh/cask/index.html ||\
- { rm -rf ~/.BREW_LIST/LOCK; exit; }
 curl -sLo ~/.BREW_LIST/master2.zip https://github.com/Homebrew/homebrew-cask-drivers/archive/master.zip ||\
  { rm -rf ~/.BREW_LIST/LOCK; exit; }
-fi
 
 /usr/bin/unzip -q ~/.BREW_LIST/master1.zip -d ~/.BREW_LIST
  ls ~/.BREW_LIST/homebrew-cask-fonts-master/Casks|\
  sed 's/\(.*\)\.rb$/homebrew\/cask-fonts\/\1/' >~/.BREW_LIST/Q_FONT.txt
 
-if [ `uname` = Darwin ];then
  /usr/bin/unzip -q ~/.BREW_LIST/master2.zip -d ~/.BREW_LIST
  ls ~/.BREW_LIST/homebrew-cask-drivers-master/Casks|\
  sed 's/\(.*\)\.rb$/homebrew\/cask-drivers\/\1/' >~/.BREW_LIST/Q_DRIV.txt
-fi
 
-if [ `uname` = Darwin ];then
- rm -f ~/.BREW_LIST/cask.txt
-  test='0'
-   LIST=()
  while read line;do
-  if [[ $line =~ ^[:blank:]*\<td\>\<a[^\>]*\>(.+)\</a\>\</td\>$ ]];then
-   LS1="${BASH_REMATCH[1]}\\t"
+  if [[ $line =~ \<td\>\<a[^\>]*\>(.+)\</a\>\</td\>$ ]];then
+   LS1=${BASH_REMATCH[1]}"\\t"
     continue
   fi
-  if [[ $line =~ ^[:blank:]*\<td\>(.+)\</td\>$ ]] && [ $test = '0' ];then
-   LS2="${BASH_REMATCH[1]}\\n"
+  if [[ $line =~ \<td\>(.+)\</td\>$ ]] && [ $test = 0 ];then
+   LS2=${BASH_REMATCH[1]}"\\n"
     test='1'
      continue
   fi
-  if [[ $line =~ ^[:blank:]*\<td\>(.+)\</td\>$ ]] && [ $test = '1' ];then
-   LS3="${BASH_REMATCH[1]}\\t"
+  if [[ $line =~ \<td\>(.+)\</td\>$ ]] && [ $test = 1 ];then
+   LS3=${BASH_REMATCH[1]}"\\t"
     test='0'
   fi
    LIST+=("$LS1$LS3$LS2")
   LS1=''; LS2=''; LS3=''
  done < ~/.BREW_LIST/Q_CASK.html
+
  echo -ne ${LIST[@]}|sed 's/^ //' > ~/.BREW_LIST/cask.txt
+
+else
+ curl -so ~/.BREW_LIST/Q_BREW.html https://formulae.brew.sh/formula-linux/index.html ||\
+  { rm -rf ~/.BREW_LIST/LOCK; exit; }
 fi
 
- rm -f ~/.BREW_LIST/brew.txt
-  test='0'
  while read line;do
-  if [[ $line =~ ^[:blank:]*\<td\>\<a[^\>]*\>(.+)\</a\>\</td\>$ ]];then
+  if [[ $line =~ \<td\>\<a[^\>]*\>(.+)\</a\>\</td\>$ ]];then
    echo -ne ${BASH_REMATCH[1]}\\t >> ~/.BREW_LIST/_brew.txt
     continue
   fi
-  if [[ $line =~ ^[:blank:]*\<td\>(.+)\</td\>$ ]] && [ $test = '0' ];then
+  if [[ $line =~ \<td\>(.+)\</td\>$ ]] && [ $test = 0 ];then
    echo -ne ${BASH_REMATCH[1]}\\t >> ~/.BREW_LIST/_brew.txt
     test='1'
      continue
   fi
-  if [[ $line =~ ^[:blank:]*\<td\>(.+)\</td\>$ ]] && [ $test = '1' ];then
-   echo -e ${BASH_REMATCH[1]} >> ~/.BREW_LIST/_brew.txt
+  if [[ $line =~ \<td\>(.+)\</td\>$ ]] && [ $test = 1 ];then
+   echo ${BASH_REMATCH[1]} >> ~/.BREW_LIST/_brew.txt
     test='0'
   fi
  done < ~/.BREW_LIST/Q_BREW.html
+
  sort ~/.BREW_LIST/_brew.txt > ~/.BREW_LIST/brew.txt 
  
-rm -f ~/.BREW_LIST/master1.zip ~/.BREW_LIST/master2.zip ~/.BREW_LIST/_brew.txt
+rm -f ~/.BREW_LIST/master1.zip ~/.BREW_LIST/master2.zip
 rm -rf ~/.BREW_LIST/homebrew-cask-fonts-master ~/.BREW_LIST/homebrew-cask-drivers-master
-rm -rf ~/.BREW_LIST/LOCK
+rm -rf ~/.BREW_LIST/_brew.txt ~/.BREW_LIST/LOCK
