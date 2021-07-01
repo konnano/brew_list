a45ce41a668fc02b93b5318ca570788cf3898514
konnano <73874687+konnano@users.noreply.github.com>
2021-07-01 21:18:28 +0900

    Update tie.pl

--- tie.pl
+++ tie.pl
@@ -30,44 +30,46 @@ tie my %tap,"NDBM_File","$ENV{'HOME'}/.BREW_LIST/DBM",O_RDWR|O_CREAT,0644;
   open my $BREW1,'<',"$dir.rb" or die " Info_1 $!\n";
    while(my $data=<$BREW1>){
 
-     if( $data =~ /^\s*bottle\s*do.*/ ){
+     if( $data =~ /^\s*bottle\s*do/ ){
       $i=1; next;
-     }elsif( $data =~ /rebuild/ and $i == 1 ){
+     }elsif( $data =~ /\s*rebuild/ and $i == 1 ){
       next;
      }elsif( $data !~ /^\s*end/ and $i == 1 ){
-       $tap{"$name$data"} = 1 if $data =~ s/.*arm64_big_sur.*\n/11.0M1/;
-        $tap{"$name$data"} = 1 if $data =~ s/.*big_sur.*\n/11.0/;
-       $tap{"$name$data"} = 1 if $data =~ s/.*catalina.*\n/10.15/;
-        $tap{"$name$data"} = 1 if $data =~ s/.*mojave.*\n/10.14/;
-       $tap{"$name$data"} = 1 if $data =~ s/.*high_sierra.*\n/10.13/;
-        $tap{"$name$data"} = 1 if $data =~ s/.*sierra.*\n/10.12/;
-       $tap{"$name$data"} = 1 if $data =~ s/.*el_capitan.*\n/10.11/;
-        $tap{"$name$data"} = 1 if $data =~ s/.*yosemite.*\n/10.10/;
-       $tap{"$name$data"} = 1 if $data =~ s/.*x86_64_linux.*\n/Linux/;
-        if( $data =~ /.*,\s*all:.*\n/ ){
+       $tap{"$name$data"} = 1 if $data =~ s/.*arm64_big_sur:.*\n/11.0M1/;
+        $tap{"$name$data"} = 1 if $data =~ s/.*big_sur:.*\n/11.0/;
+       $tap{"$name$data"} = 1 if $data =~ s/.*catalina:.*\n/10.15/;
+        $tap{"$name$data"} = 1 if $data =~ s/.*mojave:.*\n/10.14/;
+       $tap{"$name$data"} = 1 if $data =~ s/.*high_sierra:.*\n/10.13/;
+        $tap{"$name$data"} = 1 if $data =~ s/.*sierra:.*\n/10.12/;
+       $tap{"$name$data"} = 1 if $data =~ s/.*el_capitan:.*\n/10.11/;
+        $tap{"$name$data"} = 1 if $data =~ s/.*yosemite:.*\n/10.10/;
+       $tap{"$name$data"} = 1 if $data =~ s/.*x86_64_linux:.*\n/Linux/;
+        if( $data =~ /.*,\s*all:/ ){
          $tap{"${name}11.0M1"} = 1;  $tap{"${name}11.0"} = 1;
           $tap{"${name}10.15"} = 1;   $tap{"${name}10.14"} = 1;
          $tap{"${name}10.13"} = 1;   $tap{"${name}10.12"} = 1;
           $tap{"${name}10.11"} = 1;   $tap{"${name}10.10"} = 1;
         }
       next;
-     }elsif( $data =~ /^\s+end/ and $i == 1 ){
+     }elsif( $data =~ /^\s*end/ and $i == 1 ){
       $i = 0; next;
      }
 
-    if( $data =~ /^\s*on_linux\s*do.*/ ){
+    if( $data =~ /^\s*on_linux\s*do/ ){
      $i = 2; next;
-    }elsif( $data !~ /^\s*end/ and $data =~ /.*keg_only.*/ and $i == 2 ){
+    }elsif( $data !~ /^\s*end/ and $data =~ /^\s*keg_only/ and $i == 2 ){
      $tap{"${name}keg_Linux"} = 1; next;
     }elsif( $data =~ /^\s*end/ and $i == 2 ){
      $i = 0; next;
     }
 
-     if( $data =~ /.*keg_only.*macos.*/ ){
+     if( $data =~ /^\s*keg_only.*macos/ ){
       $tap{"${name}keg"} = 1; next;
+     }elsif( $data =~ /^\s*keg_only/ ){
+      $tap{"${name}keg_Linux"} = $tap{"${name}keg"} = 1; next;
+     }elsif( $data =~ /^\s*depends_on\s*:macos/ ){
+      $tap{"${name}un_Linux"} = 1; next;
      }
-     $tap{"${name}keg_Linux"} = $tap{"${name}keg"} = 1 if $data =~ /.*keg_only.*/;
-    $tap{"${name}un_Linux"} = 1  if $data =~ /.*depends_on\s*:macos.*/;
    }
   close $BREW1;
  }
