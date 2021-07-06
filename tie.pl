34dac75e1731d9a157cddf5ea0477d061861a1a7
konnano <73874687+konnano@users.noreply.github.com>
2021-07-06 23:50:09 +0900

    Update tie.pl

--- tie.pl
+++ tie.pl
@@ -8,15 +8,15 @@ my $i = 0;
 my( $OS_Version,%MAC_OS );
 
 if( $^O eq 'darwin' ){
-$OS_Version = `sw_vers -productVersion`;
-$OS_Version =~ s/(\d\d.\d+)\.?\d*\n/$1/;
-%MAC_OS = ('catalina'=>'10.15','mojave'=>'10.14','high_sierra'=>'10.13',
-              'sierra'=>'10.12','el_capitan'=>'10.11','yosemite'=>'10.10');
+ $OS_Version = `sw_vers -productVersion`;
+  $OS_Version =~ s/(\d\d.\d+)\.?\d*\n/$1/;
+ %MAC_OS = ('catalina'=>'10.15','mojave'=>'10.14','high_sierra'=>'10.13',
+            'sierra'=>'10.12','el_capitan'=>'10.11','yosemite'=>'10.10');
+ Dirs_1( '/usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula',0 );
+  Dirs_1( '/usr/local/Homebrew/Library/Taps',1 );
+}else{
+ Dirs_1( '/home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/homebrew/homebrew-core/Formula',0 );
 }
-
-$^O eq 'darwin' ?
-Dirs_1( '/usr/local/Homebrew/Library/Taps/homebrew/homebrew-core/Formula',0 ) :
-Dirs_1( '/home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/homebrew/homebrew-core/Formula',0 );
 
 sub Dirs_1{
  my( $dir,$ls ) = @_;
@@ -28,7 +28,6 @@ sub Dirs_1{
     }
   }
 }
-Dirs_1( '/usr/local/Homebrew/Library/Taps',1 ) if $^O eq 'darwin';
 
 tie my %tap,"NDBM_File","$ENV{'HOME'}/.BREW_LIST/DBM",O_RDWR|O_CREAT,0644;
  for my $dir(@brew){ chomp $dir;
@@ -36,7 +35,7 @@ tie my %tap,"NDBM_File","$ENV{'HOME'}/.BREW_LIST/DBM",O_RDWR|O_CREAT,0644;
   open my $BREW1,'<',$dir or die " Info_1 $!\n";
    while(my $data=<$BREW1>){
 
-     if( $data =~ /^\s*bottle\s*do/ ){
+     if( $data =~ /^\s*bottle\s+do/ ){
       $i=1; next;
      }elsif( $data =~ /\s*rebuild/ and $i == 1 ){
       next;
@@ -50,7 +49,7 @@ tie my %tap,"NDBM_File","$ENV{'HOME'}/.BREW_LIST/DBM",O_RDWR|O_CREAT,0644;
        $tap{"$name$data"} = 1 if $data =~ s/.*el_capitan:.*\n/10.11/;
         $tap{"$name$data"} = 1 if $data =~ s/.*yosemite:.*\n/10.10/;
        $tap{"$name$data"} = 1 if $data =~ s/.*x86_64_linux:.*\n/Linux/;
-        if( $data =~ /.*,\s*all:/ ){
+        if( $data =~ /.*,\s+all:/ ){
          $tap{"${name}11.0M1"} = 1;  $tap{"${name}11.0"} = 1;
           $tap{"${name}10.15"} = 1;   $tap{"${name}10.14"} = 1;
          $tap{"${name}10.13"} = 1;   $tap{"${name}10.12"} = 1;
@@ -61,7 +60,7 @@ tie my %tap,"NDBM_File","$ENV{'HOME'}/.BREW_LIST/DBM",O_RDWR|O_CREAT,0644;
       $i = 0; next;
      }
 
-    if( $data =~ /^\s*on_linux\s*do/ ){
+    if( $data =~ /^\s*on_linux\s+do/ ){
      $i = 2; next;
     }elsif( $data !~ /^\s*end/ and $data =~ /^\s*keg_only/ and $i == 2 ){
      $tap{"${name}keg_Linux"} = 1; next;
@@ -73,19 +72,20 @@ tie my %tap,"NDBM_File","$ENV{'HOME'}/.BREW_LIST/DBM",O_RDWR|O_CREAT,0644;
       $tap{"${name}keg"} = 1; next;
      }elsif( $data =~ /^\s*keg_only/ ){
       $tap{"${name}keg_Linux"} = $tap{"${name}keg"} = 1; next;
-     }elsif( $data =~ /^\s*depends_on\s*:macos/ ){
+     }elsif( $data =~ /^\s*depends_on\s+:macos/ ){
       $tap{"${name}un_Linux"} = 1; $tap{"${name}Linux"} = 0; next;
-     }elsif( $data =~ s/^\s*depends_on\s*macos:\s*:([^\s]*).*\n/$1/ ){
+     }elsif( $data =~ s/^\s*depends_on\s+macos:\s+:([^\s]*).*\n/$1/ ){
       if( $OS_Version and $MAC_OS{$data} gt $OS_Version ){
        $tap{"${name}un_xcode"} = 1; next;
       }
      }
 
-    if( $^O eq 'darwin' and $data =~ s/^\s*depends_on\s*xcode:.*"([^"]+)".*\n/$1/ ){
+    if( $^O eq 'darwin' and $data =~ s/^\s*depends_on\s+xcode:.*"([^"]+)".*\n/$1/ ){
      my $xcode = `xcodebuild -version|xargs|awk '{print \$2}'`;
       $data =~ s/(\d+\.\d+)\.?\d*/$1/;
-       $tap{"${name}un_xcode"} = 1 if $xcode < $data;
+       $tap{"${name}un_xcode"} = 1 if $data > $xcode;
     }
+
    }
   close $BREW1;
  }
