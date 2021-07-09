647185c0a3506d12463c95405c7a6ed1853e265a
konnano <73874687+konnano@users.noreply.github.com>
2021-07-09 14:03:02 +0900

    Update tie.pl

--- tie.pl
+++ tie.pl
@@ -11,9 +11,9 @@ if( $^O eq 'darwin' ){
  $OS_Version = `sw_vers -productVersion`;
   $OS_Version =~ s/(\d\d.\d+)\.?\d*\n/$1/;
  $CPU = `sysctl machdep.cpu.brand_string`;
-  $CPU = $CPU =~ /Apple\sM1/ ? 'arm\?' : 'intel\?';
+  $CPU = $CPU =~ /Apple\s+M1/ ? 'arm\?' : 'intel\?';
  $Xcode = `xcodebuild -version|xargs|awk '{print \$2}'`;
-  $Xcode =~ s/(\d+\.\d+)\.?\n\d*/$1/;
+  $Xcode =~ s/(\d+\.\d+)\.?\d*\n/$1/;
 
  %MAC_OS = ('catalina'=>'10.15','mojave'=>'10.14','high_sierra'=>'10.13',
             'sierra'=>'10.12','el_capitan'=>'10.11','yosemite'=>'10.10');
@@ -68,7 +68,7 @@ tie my %tap,"NDBM_File","$ENV{'HOME'}/.BREW_LIST/DBM",O_RDWR|O_CREAT,0644;
 
     if( $data =~ /^\s*on_linux\s+do/ ){
      $IN = 2; next;
-    }elsif( $data !~ /^\s*end/ and $data =~ /^\s*keg_only/ and $IN == 2 ){
+    }elsif( $data =~ /^\s*keg_only/ and $IN == 2 ){
      $tap{"${name}keg_Linux"} = 1; next;
     }elsif( $data =~ /^\s*end/ and $IN == 2 ){
      $IN = 0; next;
