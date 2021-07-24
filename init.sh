c40a46c17dca9dd67e34d09e03f15744bb2b6dbf
konnano <73874687+konnano@users.noreply.github.com>
2021-07-24 20:54:57 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-if [ `uname` = Darwin -a $1 -a $1 = unlink ];then
- rm -f /usr/local/bin/brew_list
-  rm -rf ~/.BREW_LIST
-   exit
-elif [ `uname` = Linux -a $1 -a $1 = unlink ];then
+CPU=`sysctl machdep.cpu.brand_string`
+NAME=`uname`
+
+if [[ ! $NAME = Darwin && ! $NAME = Linux ]];then
+ exit
+fi
+
+if [[ $NAME = Darwin && $1 && $1 = unlink ]];then
+  if [[ $CPU =~ Intel ]];then
+   rm -f /usr/local/bin/brew_list
+  elif [[ $CPU =~ M1 ]];then
+   rm -f /opt/homebrew/bin/brew_list
+  fi
+   rm -rf ~/.BREW_LIST
+    exit
+elif [[ $NAME = Linux && $1 && $1 = unlink ]];then
  rm -f /home/linuxbrew/.linuxbrew/bin/brew_list
   rm -rf ~/.BREW_LIST
    exit
 fi
 
-if [ `uname` = Darwin -a -e '/usr/local/bin/brew_list' ];then
- echo exist /usr/local/bin/brew_list
-  exit
-elif [ `uname` = Linux -a -e '/home/linuxbrew/.linuxbrew/bin/brew_list' ];then
+if [[ $NAME = Darwin ]];then
+  if [[ $CPU =~ Intel &&  -e '/usr/local/bin/brew_list' ]];then
+   echo exist /usr/local/bin/brew_list  
+    exit
+  elif [[ $CPU =~ M1 && -e '/opt/homebrew/bin/brew_list' ]];then 
+   echo exist /opt/homebrew/bin/brew_list
+    exit
+  fi
+elif [[ $NAME = Linux && -e '/home/linuxbrew/.linuxbrew/bin/brew_list' ]];then
  echo exist /home/linuxbrew/.linuxbrew/bin/brew_list
   exit
 fi
@@ -22,10 +38,14 @@ DIR=$(cd $(dirname $0); pwd)
 
 mkdir -p ~/.BREW_LIST
 
-if [ `uname` = Darwin ];then
- cp $DIR/brew_list.pl /usr/local/bin/brew_list
-  cp $DIR/font.sh ~/.BREW_LIST/font.sh
-   cp $DIR/tie.pl ~/.BREW_LIST/tie.pl
+if [[ $NAME = Darwin ]];then
+  if [[ $CPU =~ Intel ]];then
+   cp $DIR/brew_list.pl /usr/local/bin/brew_list
+  elif [[ $CPU =~ M1 ]];then
+   cp $DIR/brew_list.pl /opt/homebrew/bin/brew_list
+  fi
+   cp $DIR/font.sh ~/.BREW_LIST/font.sh
+    cp $DIR/tie.pl ~/.BREW_LIST/tie.pl
 elif [ `uname` = Linux ];then
  cp $DIR/brew_list.pl /home/linuxbrew/.linuxbrew/bin/brew_list
   cp $DIR/font.sh ~/.BREW_LIST/font.sh
