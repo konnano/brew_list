c6783d7d3dc480a57cd67aa2b9c56176e4bd3030
konnano <73874687+konnano@users.noreply.github.com>
2021-08-19 13:36:41 +0900

    Update font_pre.pl

--- font_pre.pl
+++ font_pre.pl
@@ -10,29 +10,23 @@ my $dir = $CPU eq 'arm' ?
  '/opt/homebrew/Library/Taps/homebrew/homebrew-cask-fonts/Casks' :
  '/usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask-fonts/Casks';
 
-my %HA; my @AN;
+my( %HA,@AN,$VER ); 
 opendir my $FONT,$dir or die " FONT $!\n";
  for my $com(readdir($FONT)){
   next if $com eq '.' or $com eq '..';
   open my $font,"$dir/$com" or die " File $!";
    my( $name ) = $com =~ /(.+)\.rb/;
    while(my $read = <$font>){
-    $read =~ s/^\s*url\s*"([^"]+(?:ttf|otf))".*\n/$1/;
-     if( $read =~ /^https:/ ){
-      $HA{$name} = $read;
-       push @AN,"$name\n";
+    $VER = $1 if $read =~ /\s*version\s+"([^"]+)".*/;
+     if( $read =~ s/^\s*url\s+"([^"]+(?:ttf|otf))".*\n/$1/ ){
+      $read =~ s/\Q#{version}\E/$VER/;
+       $HA{$name} = $read;
+        push @AN,"$name\n";
      }
    }
+  close $font;
  }
 closedir $FONT;
-
-die " exisit master.ttf\n" if -f 'master.ttf';
- die " exisit Array.txt\n" if -f 'Array.txt';
-
- @AN = sort{$a cmp $b}@AN;
-open my $FI,'>','Array.txt' or die " Array $!\n";
- print $FI @AN;
-close $FI;
 
 my $fzf = `if type fzf >/dev/null 2>&1;then
 echo fzf
@@ -43,6 +37,15 @@ echo percol
 else
 exit
 fi`;
+exit unless $fzf;
+
+die " exisit master.ttf\n" if -f 'master.ttf';
+ die " exisit Array.txt\n" if -f 'Array.txt';
+
+ @AN = sort{$a cmp $b}@AN;
+open my $FI,'>','Array.txt' or die " Array $!\n";
+ print $FI @AN;
+close $FI;
 
 chomp( my $an = `cat Array.txt|$fzf` );
  print" $an\n";
