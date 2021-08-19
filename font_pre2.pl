94b5946cf8cd0279fb407e7f5495110624818c18
konnano <73874687+konnano@users.noreply.github.com>
2021-08-19 23:51:55 +0900

    Update font_pre2.pl

--- font_pre2.pl
+++ font_pre2.pl
@@ -1,9 +1,13 @@
 use strict;
 use warnings;
 
-# ループで Cask フォントを QuickLook 表示します、終了は Ctl+c
+# ループで Cask フォントを QuickLook 表示します
 # パッケージフォントに対応しません、単体フォントのみ、試作品です
+# 終了は QuickLook で Spaceキー 親プロセス sleep 待ちで少し時間かかります
+
 my $TIME = 6; ### 表示タイム　通信環境で設定して下さい
+$SIG{'HUP'} = $SIG{'TERM'} = $SIG{'PIPE'} = $SIG{'INT'} = 'exit_1';
+sub exit_1{ sleep 1; rmdir './FONT_EXIT'; unlink './master.ttf'; exit; }
 
 my $CPU = `sysctl machdep.cpu.brand_string`;
 $CPU = $CPU =~ /Apple\s+M1/ ? 'arm\?' : 'intel\?';
@@ -30,25 +34,35 @@ opendir my $FONT,$dir or die " FONT $!\n";
 closedir $FONT;
 
 die " exisit master.ttf\n" if -f 'master.ttf';
- shuf_1(\@AN);
+ die " exisit FONT_EXIT\n" if -d 'FONT_EXIT';
+  shuf_1(\@AN);
 
 for my $an( @AN ){
- print"$an";
-  chomp $an;
+ if( -d './FONT_EXIT' ){
+  rmdir './FONT_EXIT';
+   exit;
+ }
+  print"$an";
+   chomp $an;
  my $pid = fork;
  die " Not fork : $!\n" unless defined $pid;
   if($pid){
    sleep $TIME;
-    system("osascript -e 'tell application \"System Events\"
-            keystroke (key code 49)
-            end tell'");
+    unless( -d './FONT_EXIT' ){
+     system("osascript -e 'tell application \"System Events\"
+             keystroke (key code 49)
+             end tell'");
+    }
   }else{
-     system("curl -sLo master.ttf $HA{$an} 2>/dev/null");
-     system('qlmanage -p master.ttf >& /dev/null');
-     exit;
+   my $time = time;
+    ( mkdir './FONT_EXIT' and die "  \033[31mNot connected\033[37m\n" )
+     if system("curl -sLo master.ttf $HA{$an}");
+      system('qlmanage -p master.ttf >& /dev/null');
+       ( print" wait\n" and mkdir './FONT_EXIT' ) if $TIME > time - $time;
+        exit;
   }
  sleep 1;
- unlink 'master.ttf';
+ unlink './master.ttf';
 }
 
 sub shuf_1{
