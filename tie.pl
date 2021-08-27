dc9257f467ae6d54e1e70aaf13091ea1e3c37836
konnano <73874687+konnano@users.noreply.github.com>
2021-08-27 16:22:49 +0900

    Update tie.pl

--- tie.pl
+++ tie.pl
@@ -170,9 +170,10 @@ tie my %tap,"NDBM_File","$ENV{'HOME'}/.BREW_LIST/DBM",O_RDWR|O_CREAT,0644;
        $IF1 = 0; $VER = 1;
       }
     }elsif( $data =~ /else/ and $IF1 and $IF2 ){
-       $IF1 = $IF2 = 0; $VER = 1;
+       $VER = 1;
     }elsif( my( $ls6 ) = $data =~ /version\s+"([^"]+)"/ and $VER ){
-     $tap{"${name}version"} = $ls6; $VER = 0;
+     $tap{"${name}version"} = $ls6;
+      $IF1 = $IF2 = $VER = 0;
     }
    $IF1 = 1 if eof;
    }
