f0435a4825a56b839f03e80f4eeba13ce28eedfa
konnano <73874687+konnano@users.noreply.github.com>
2021-10-26 23:16:39 +0900

    Update tie.pl

--- tie.pl
+++ tie.pl
@@ -12,6 +12,7 @@ if( $^O eq 'darwin' ){
   $OS_Version =~ s/^(10.1\d)\.?\d*\n/$1/;
    $OS_Version =~ s/^(10\.)([7-9])\.?\d*\n/${1}0$2/;
     $OS_Version =~ s/^11.+\n/11.0/;
+     $OS_Version =~ s/^12.+\n/12.0/;
 
  $CPU = `sysctl machdep.cpu.brand_string`;
   $CPU = $CPU =~ /Apple\s+M1/ ? 'arm\?' : 'intel\?';
@@ -22,8 +23,8 @@ if( $^O eq 'darwin' ){
   `xcodebuild -version|awk '/Xcode/{print \$NF}'` : 0;
     $Xcode =~ s/^(\d\.)/0$1/;
 
- %MAC_OS = ('big_sur'=>'11.0','catalina'=>'10.15','mojave'=>'10.14','high_sierra'=>'10.13',
-            'sierra'=>'10.12','el_capitan'=>'10.11','yosemite'=>'10.10');
+ %MAC_OS = ('monterey'=>'12.0','big_sur'=>'11.0','catalina'=>'10.15','mojave'=>'10.14',
+            'high_sierra'=>'10.13','sierra'=>'10.12','el_capitan'=>'10.11','yosemite'=>'10.10');
  $re->{'CLANG'} = `clang --version|awk '/Apple/'|sed 's/.*-\\([^.]*\\)\\..*/\\1/'`;
  $re->{'CLT'} = `pkgutil --pkg-info=com.apple.pkg.CLTools_Executables|\
                  awk '/version/{print \$2}'|sed 's/\\([0-9]*\\.[0-9]*\\).*/\\1/'`;
@@ -76,16 +77,19 @@ tie my %tap,"NDBM_File","$ENV{'HOME'}/.BREW_LIST/DBM",O_RDWR|O_CREAT,0644;
      }elsif( $data =~ /^\s*rebuild/ and $IN == 1 ){
        next;
      }elsif( $data !~ /^\s*end/ and $IN == 1 ){
-       $tap{"$name$data"} = 1 if $data =~ s/.*arm64_big_sur:.*\n/11.0M1/;
-       $tap{"$name$data"} = 1 if $data =~ s/.*big_sur:.*\n/11.0/;
-       $tap{"$name$data"} = 1 if $data =~ s/.*catalina:.*\n/10.15/;
-       $tap{"$name$data"} = 1 if $data =~ s/.*mojave:.*\n/10.14/;
-       $tap{"$name$data"} = 1 if $data =~ s/.*high_sierra:.*\n/10.13/;
-       $tap{"$name$data"} = 1 if $data =~ s/.*sierra:.*\n/10.12/;
-       $tap{"$name$data"} = 1 if $data =~ s/.*el_capitan:.*\n/10.11/;
-       $tap{"$name$data"} = 1 if $data =~ s/.*yosemite:.*\n/10.10/;
-       $tap{"$name$data"} = 1 if $data =~ s/.*x86_64_linux:.*\n/Linux/;
+       $data =~ s/.*arm64_monterey:.*\n/12.0M1/ ? $tap{"$name$data"} = 1 :
+       $data =~ s/.*monterey:.*\n/12.0/         ? $tap{"$name$data"} = 1 :
+       $data =~ s/.*arm64_big_sur:.*\n/11.0M1/  ? $tap{"$name$data"} = 1 :
+       $data =~ s/.*big_sur:.*\n/11.0/          ? $tap{"$name$data"} = 1 :
+       $data =~ s/.*catalina:.*\n/10.15/        ? $tap{"$name$data"} = 1 :
+       $data =~ s/.*mojave:.*\n/10.14/          ? $tap{"$name$data"} = 1 :
+       $data =~ s/.*high_sierra:.*\n/10.13/     ? $tap{"$name$data"} = 1 :
+       $data =~ s/.*sierra:.*\n/10.12/          ? $tap{"$name$data"} = 1 :
+       $data =~ s/.*el_capitan:.*\n/10.11/      ? $tap{"$name$data"} = 1 :
+       $data =~ s/.*yosemite:.*\n/10.10/        ? $tap{"$name$data"} = 1 :
+       $data =~ s/.*x86_64_linux:.*\n/Linux/    ? $tap{"$name$data"} = 1 : 0;
         if( $data =~ /.*,\s+all:/ ){
+         $tap{"${name}12.0M1"} = $tap{"${name}12.0"} = 
          $tap{"${name}11.0M1"} = $tap{"${name}11.0"} = $tap{"${name}10.15"} =
          $tap{"${name}10.14"} = $tap{"${name}10.13"} = $tap{"${name}10.12"} =
          $tap{"${name}10.11"} = $tap{"${name}10.10"} = $tap{"${name}Linux"} =
