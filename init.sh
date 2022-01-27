491e7ca2665ae81179e9725749b82b68af30bdc1
konnano <73874687+konnano@users.noreply.github.com>
2022-01-27 18:32:22 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
- NAME=`uname`
+NAME=$(uname)
 if [[ ! $NAME = Darwin && ! $NAME = Linux ]];then
  exit
 elif [[ $NAME = Darwin ]];then
- VER=`sw_vers -productVersion|sed -E 's/^(1[1-2]).*/\1.0/'|\
-                              sed -E 's/^(10\.)(1[0-5]).*/\1\2/'|\
-                              sed -E 's/^(10\.)([0-9])($|\.).*/\10\2/'`
- [[ 10.09 > $VER ]] && echo ' Use Tiger Brew' && exit
- CPU=`uname -m`
+ VER=$(sw_vers -productVersion|sed -E 's/^(1[1-2]).*/\1.0/'|\
+                               sed -E 's/^(10\.)(1[0-5]).*/\1\2/'|\
+                               sed -E 's/^(10\.)([0-9])($|\.).*/\10\2/')
+ [[ 10.09 > "$VER" ]] && echo ' Use Tiger Brew' && exit
+ CPU=$(uname -m)
 fi
 
 if [[ $NAME = Darwin && $1 = unlink ]];then
- if [[ $CPU =~ x86_64 ]];then
-  rm -f /usr/local/bin/brew_list
- else
-  rm -f /opt/homebrew/bin/brew_list
- fi
+ [[ $CPU = x86_64 ]] && rm -f /usr/local/bin/brew_list || rm -f /opt/homebrew/bin/brew_list
   rm -rf ~/.BREW_LIST
    echo rm all cache
     exit
@@ -27,16 +23,17 @@ fi
 
 if [[ ! $1 ]];then
  if [[ $NAME = Darwin ]];then
-  if [[ $CPU =~ x86_64 &&  -e /usr/local/bin/brew_list ]];then
-   echo exist /usr/local/bin/brew_list  
-    exit
-  elif [[ $CPU =~ arm64 && -e /opt/homebrew/bin/brew_list ]];then 
-   echo exist /opt/homebrew/bin/brew_list
-    exit
+  if [[ $CPU = x86_64 ]];then
+   [[ ! -d /usr/local/Cellar ]] && echo " Not installed HOME BREW" && exit
+   [[ -e /usr/local/bin/brew_list ]] && echo " exist /usr/local/bin/brew_list" && exit
+  else
+   [[ ! -d /opt/homebrew/Cellar ]] && echo " Not installed HOME BREW" && exit
+   [[  -e /opt/homebrew/bin/brew_list ]] && echo " exist /opt/homebrew/bin/brew_list" && exit
   fi
- elif [[ $NAME = Linux && -e /home/linuxbrew/.linuxbrew/bin/brew_list ]];then
-  echo exist /home/linuxbrew/.linuxbrew/bin/brew_list
-   exit
+ else
+  [[ ! -d /home/linuxbrew/.linuxbrew/Cellar ]] && echo " Not installed HOME BREW" && exit
+  [[ -e /home/linuxbrew/.linuxbrew/bin/brew_list ]] && \
+   echo " exist /home/linuxbrew/.linuxbrew/bin/brew_list" && exit
  fi
 
  curl -k https://formulae.brew.sh/formula >/dev/null 2>&1 || \
@@ -44,11 +41,8 @@ if [[ ! $1 ]];then
  DIR=$(cd $(dirname $0); pwd)
 
  if [[ $NAME = Darwin ]];then
-  if [[ $CPU =~ x86_64 ]];then
-   cp $DIR/brew_list.pl /usr/local/bin/brew_list
-  else
+  [[ $CPU = x86_64 ]] && cp $DIR/brew_list.pl /usr/local/bin/brew_list || \
    cp $DIR/brew_list.pl /opt/homebrew/bin/brew_list
-  fi
  else
   cp $DIR/brew_list.pl /home/linuxbrew/.linuxbrew/bin/brew_list
  fi
