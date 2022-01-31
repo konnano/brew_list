320a19f76e720b765a9fb7c64015c9ed57a4b32d
konnano <73874687+konnano@users.noreply.github.com>
2022-01-31 20:13:23 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-NAME=$(uname)
-if [[ ! $NAME = Darwin && ! $NAME = Linux ]];then
+ NAME=$(uname)
+if [[ ! "$NAME" = Darwin && ! "$NAME" = Linux ]];then
  exit
-elif [[ $NAME = Darwin ]];then
+elif [[ "$NAME" = Darwin ]];then
  VER=$(sw_vers -productVersion|sed -E 's/^(1[1-2]).*/\1.0/'|\
                                sed -E 's/^(10\.)(1[0-5]).*/\1\2/'|\
                                sed -E 's/^(10\.)([0-9])($|\.).*/\10\2/')
  [[ 10.09 > "$VER" ]] && echo ' Use Tiger Brew' && exit
  CPU=$(uname -m)
 fi
-
-if [[ $NAME = Darwin && $1 = unlink ]];then
- [[ $CPU = x86_64 ]] && rm -f /usr/local/bin/brew_list || rm -f /opt/homebrew/bin/brew_list
+ LINK="$1"
+if [[ "$NAME" = Darwin && "$LINK" = unlink ]];then
+ [[ "$CPU" = x86_64 ]] && rm -f /usr/local/bin/brew_list || rm -f /opt/homebrew/bin/brew_list
   rm -rf ~/.BREW_LIST
    echo rm all cache
     exit
-elif [[ $NAME = Linux && $1 = unlink ]];then
+elif [[ "$NAME" = Linux && "$LINK" = unlink ]];then
   rm -rf /home/linuxbrew/.linuxbrew/bin/brew_list ~/.BREW_LIST
    echo rm all cache
     exit
 fi
 
-if [[ ! $1 ]];then
- if [[ $NAME = Darwin ]];then
-  if [[ $CPU = x86_64 ]];then
+if [[ ! "$LINK" ]];then
+ if [[ "$NAME" = Darwin ]];then
+  if [[ "$CPU" = x86_64 ]];then
    [[ ! -d /usr/local/Cellar ]] && echo " Not installed HOME BREW" && exit
    [[ -e /usr/local/bin/brew_list ]] && echo " exist /usr/local/bin/brew_list" && exit
   else
@@ -40,8 +40,8 @@ if [[ ! $1 ]];then
   { echo -e "\033[31m Not connected\033[00m"; exit; }
  DIR=$(cd $(dirname $0); pwd)
 
- if [[ $NAME = Darwin ]];then
-  [[ $CPU = x86_64 ]] && { cp $DIR/brew_list.pl /usr/local/bin/brew_list || ${die?copy error}; } || \
+ if [[ "$NAME" = Darwin ]];then
+  [[ "$CPU" = x86_64 ]] && { cp $DIR/brew_list.pl /usr/local/bin/brew_list || ${die?copy error}; } ||\
    { cp $DIR/brew_list.pl /opt/homebrew/bin/brew_list || ${die?copy error}; }
  else
   cp $DIR/brew_list.pl /home/linuxbrew/.linuxbrew/bin/brew_list || ${die?copy error}
