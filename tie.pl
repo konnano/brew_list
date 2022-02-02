0b24b5efc6a31ab3b4038d31a19707c86433a632
konnano <73874687+konnano@users.noreply.github.com>
2022-02-02 19:47:58 +0900

    Update tie.pl

--- tie.pl
+++ tie.pl
@@ -24,9 +24,11 @@ if( $^O eq 'darwin' ){
 
  %MAC_OS = ('monterey'=>'12.0','big_sur'=>'11.0','catalina'=>'10.15','mojave'=>'10.14',
             'high_sierra'=>'10.13','sierra'=>'10.12','el_capitan'=>'10.11','yosemite'=>'10.10');
- $re->{'CLANG'} = `clang --version|awk '/Apple/'|sed 's/.*-\\([^.]*\\)\\..*/\\1/'`;
- $re->{'CLT'} = `pkgutil --pkg-info=com.apple.pkg.CLTools_Executables|\
-                 awk '/version/{print \$2}'|sed 's/\\([0-9]*\\.[0-9]*\\).*/\\1/'`;
+ $re->{'CLANG'} = `clang --version|sed '/Apple/!d' 2>/dev/null` ?
+                  `clang --version|sed -n '/Apple/s/.*clang-\\([^.]*\\).*/\\1/p'` : 0;
+ $re->{'CLT'} = `pkgutil --pkg-info=com.apple.pkg.CLTools_Executables 2>/dev/null` ?
+                `pkgutil --pkg-info=com.apple.pkg.CLTools_Executables|\
+                 sed -n '/version/s/[^0-9]*\\([0-9]*\\.[0-9]*\\).*/\\1/p'` : 0;
 
   if( $CPU eq 'intel\?' ){
    Dirs_1( '/usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask/Casks',0,1 );
@@ -42,8 +44,8 @@ if( $^O eq 'darwin' ){
  rmdir "$ENV{'HOME'}/.BREW_LIST/7";
 }else{
  $re->{'LIN'} = 1;
-  $RPM = `ldd --version|awk '/ldd/{print \$NF}'`;
-   $CAT = `cat ~/.BREW_LIST/brew.txt|awk '/glibc/{print \$2}'`;
+  $RPM = `ldd --version 2>/dev/null` ? `ldd --version|awk '/ldd/{print \$NF}'` : 0;
+   $CAT = `cat ~/.BREW_LIST/brew.txt 2>/dev/null` ? `cat ~/.BREW_LIST/brew.txt|awk '/glibc/{print \$2}'` : 0;
     $OS_Version2 = $UNAME =~ /x86_64/ ? 'Linux' : $UNAME =~ /arm64/ ? 'LinuxM1' : 'Linux32';
 
  Dirs_1( '/home/linuxbrew/.linuxbrew/Homebrew/Library/Taps/homebrew/homebrew-core/Formula',0,0 );
@@ -88,7 +90,7 @@ tie my %tap,"NDBM_File","$ENV{'HOME'}/.BREW_LIST/DBMG",O_RDWR|O_CREAT,0644 or di
        $data =~ s/.*yosemite:.*\n/10.10/        ? 1 :
        $data =~ s/.*x86_64_linux:.*\n/Linux/    ? 1 : 0; # x86_64
         if( $data =~ /.*,\s+all:/ ){
-         $tap{"${name}12.0M1"} = $tap{"${name}12.0"} = 
+         $tap{"${name}12.0M1"} = $tap{"${name}12.0"} =
          $tap{"${name}11.0M1"} = $tap{"${name}11.0"} = $tap{"${name}10.15"} =
          $tap{"${name}10.14"} = $tap{"${name}10.13"} = $tap{"${name}10.12"} =
          $tap{"${name}10.11"} = $tap{"${name}10.10"} = $tap{"${name}10.09"} =
@@ -166,7 +168,7 @@ tie my %tap,"NDBM_File","$ENV{'HOME'}/.BREW_LIST/DBMG",O_RDWR|O_CREAT,0644 or di
           $tap{"${name}un_Linux"} = 1;
            $tap{"${name}un_Linux"} = 0 if $tap{"${name}Linux"};
          } next;
-      }elsif( my( $ls3,$ls4 ) = 
+      }elsif( my( $ls3,$ls4 ) =
         $data =~ /^\s*depends_on\s+xcode:.+:build.+if\s+MacOS\.version\s+([^\s]+)\s+:([^\s]+)/ ){
          if( $re->{'MAC'} and eval "$OS_Version $ls3 $MAC_OS{$ls4}" and not $Xcode ){
           $tap{"${name}un_xcode"} = 1;
@@ -270,7 +272,7 @@ tie my %tap,"NDBM_File","$ENV{'HOME'}/.BREW_LIST/DBMG",O_RDWR|O_CREAT,0644 or di
           $tap{"${ls4}uses"} .= "$name\t";
          }
        }elsif( my( $ls7,$ls8 ) =
-        $data =~ /^\s*depends_on\s+"([^"]+)"\s+if\s+Hardware::CPU\.([^\s]+)/ ){	
+        $data =~ /^\s*depends_on\s+"([^"]+)"\s+if\s+Hardware::CPU\.([^\s]+)/ ){
           $tap{"${ls7}uses"} .= "$name\t" if $ls8 =~ /$CPU/;
        }
      }
