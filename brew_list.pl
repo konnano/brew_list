73b4955068ca574c18fccdd82ee4e738de2192f0
konnano <73874687+konnano@users.noreply.github.com>
2022-10-08 00:15:57 +0900

    Update brew_list.pl

--- brew_list.pl
+++ brew_list.pl
@@ -197,7 +197,7 @@ sub Died_1{ my $Lang;
    # Uninstall rm -rf ~/.BREW_LIST ~/.JA_BREW ; Then brew uninstall brew_list\n" :
    "\n   # Uninstall rm -rf ~/.BREW_LIST ; Then brew uninstall brew_list\n";
   }
-  print"  Enhanced brew list : version 1.12_9\n   Option\n  -new\t:  creat new cache
+  print"  Enhanced brew list : version 1.12_8\n   Option\n  -new\t:  creat new cache
   -l\t:  formula list : First argument Formula search : Second argument '.' Full-text search
   -i\t:  instaled formula list\n  -\t:  brew list command\n  -lb\t:  bottled install formula list
   -lx\t:  can't install formula list\n  -s\t:  type search formula name\n  -o\t:  brew outdated
