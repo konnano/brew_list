2cc8b4904e19f80b5ff85ce12dbb0cb7fde1b2bb
konnano <73874687+konnano@users.noreply.github.com>
2022-10-17 21:56:35 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,14 +4,14 @@ if [[ ! "$NAME" = Darwin && ! "$NAME" = Linux ]];then
  ${die:?Not support OS}
 elif [[ "$NAME" = Darwin ]];then
  VER=$(sw_vers -productVersion|\
- sed -E 's/^(1[1-2]).*/\1.0/;s/^(10\.)(1[0-5]).*/\1\2/;s/^(10\.)([0-9])($|\.).*/\10\2/')
+ sed -E 's/^(1[1-2]).*/\1.0/;s/^(10\.)(1[0-5]).*/\1\2/;s/^(10\.)([1-9])($|\.).*/\10\2/')
  [[ 10.09 > "$VER" ]] && echo ' Use Tiger Brew' && exit 1
  CPU=$(uname -m)
 fi
  LINK="$1"
 if [[ "$NAME" = Darwin && "$LINK" = unlink ]];then
- [[ "$CPU" = x86_64 ]] && rm -f /usr/local/bin/bl /usr/local/share/zsh/site-functions/_bl ||\
-  rm -f /opt/homebrew/bin/bl /opt/homebrew/share/zsh/site-functions/_bl
+ [[ "$CPU" = x86_64 ]] && rm -f /usr/local/bin/{brew_list,bl} /usr/local/share/zsh/site-functions/_bl ||\
+                          rm -f /opt/homebrew/bin/{brew_list,bl} /opt/homebrew/share/zsh/site-functions/_bl
   rm -rf ~/.BREW_LIST ~/.JA_BREW
    echo rm all cache
     exit
@@ -34,12 +34,12 @@ if [[ ! "$LINK" || "$LINK" = JA ]];then
   fi
  else
   [[ ! -d /home/linuxbrew/.linuxbrew/Cellar ]] && echo " Not installed HOME BREW" && exit
-  [[ -e /home/linuxbrew/.linuxbrew/bin/bl ]] && \
+  [[ -e /home/linuxbrew/.linuxbrew/bin/bl ]] &&\
    echo " exist /home/linuxbrew/.linuxbrew/bin/bl" && exit
     BREW="/home/linuxbrew/.linuxbrew/bin/bl"
  fi
 
- curl -k https://formulae.brew.sh/formula >/dev/null 2>&1 || \
+ curl -k https://formulae.brew.sh/formula >/dev/null 2>&1 ||\
   { echo -e "\033[31m Not connected\033[00m"; exit 1; }
  trap 'rm -f "$BREW"; exit 1' 1 2 3 15
  DIR=$(cd $(dirname $0); pwd)
