68cc53138d2462c3ed5b7558ac11653e3f75f0ae
konnano <73874687+konnano@users.noreply.github.com>
2022-12-12 22:12:14 +0900

    Update tie.pl

--- tie.pl
+++ tie.pl
@@ -7,7 +7,7 @@ my( $IN,$KIN,$SPA ) = ( 0,0,0 );
 my $UNAME = `uname -m` !~ /arm64|aarch64/ ? 'x86_64' : 'arm64';
 my $CPU = $UNAME =~ /arm64/ ? 'arm\?' : 'intel\?';
 my( $re,$OS_Version,$OS_Version2,%MAC_OS,%HAN,$Xcode,$RPM,$CAT,@BREW,@CASK );
-chomp( my $MY_BREW = `dirname \$(dirname \$(which brew))` );
+chomp( my $MY_BREW = `dirname \$(dirname \$(which brew) 2>/dev/null) 2>/dev/null` );
 
 if( $^O eq 'darwin' ){ $re->{'MAC'} = 1;
  $OS_Version = `sw_vers -productVersion`;
@@ -235,7 +235,7 @@ unless( $ARGV[0] ){
       }
      }elsif( $re->{'LIN'} and
              my( $us7 ) = $data =~ /^\s*depends_on\s+"([^"]+)".*\["glibc"]\.any_version_installed/ ){
-      if( `which /home/linuxbrew/.linuxbrew/bin/glibc 2>/dev/null` ){
+      if( `which /home/linuxbrew/.linuxbrew/bin/glibc` ){
        $tap{"${us7}uses"} .= "$name\t";
         $tap{"${name}deps"} .= "$us7\t";
       }
